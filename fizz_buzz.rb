0f3d06c6ae8aa43fdc25de87019880a52344fac0
sasakama1984 <ideal_gate@icloud.com>
2020-04-05 16:05:44 +0000

    [Update]テストを追加

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -13,10 +13,14 @@ end
 require 'minitest/autorun'
 
 class FizzBuzzTest < Minitest::Test
-	def test_fizz_buzz
+	def testFizz_buzz
 		assert_equal '1', fizz_buzz(1)
 		assert_equal '2', fizz_buzz(2)
 		assert_equal 'Fizz', fizz_buzz(3)
+		assert_equal '4', fizz_buzz(4)
+		assert_equal 'Buzz', fizz_buzz(5)
+		assert_equal 'Fizz', fizz_buzz(6)
+		assert_equal 'FizzBuzz', fizz_buzz(15)
 	end
 end
 # puts fizz_buzz(1)
