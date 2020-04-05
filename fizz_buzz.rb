04bd985918b27e636446a5acb4b878876c6f5a75
sasakama1984 <ideal_gate@icloud.com>
2020-04-05 15:25:03 +0000

    [Update]add minitest

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -9,10 +9,20 @@ def fizz_buzz(n)
 		n.to_s
 	end
 end
-puts fizz_buzz(1)
-puts fizz_buzz(2)
-puts fizz_buzz(3)
-puts fizz_buzz(4)
-puts fizz_buzz(5)
-puts fizz_buzz(6)
-puts fizz_buzz(15)
+
+require 'minitest/autorun'
+
+class FizzBuzzTest < Minitest::Test
+	def test_fizz_buzz
+		assert_equal '1', fizz_buzz(1)
+		assert_equal '2', fizz_buzz(2)
+		assert_equal 'Fizz', fizz_buzz(3)
+	end
+end
+# puts fizz_buzz(1)
+# puts fizz_buzz(2)
+# puts fizz_buzz(3)
+# puts fizz_buzz(4)
+# puts fizz_buzz(5)
+# puts fizz_buzz(6)
+# puts fizz_buzz(15)
